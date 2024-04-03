ef8512643e6524227f2d385d86aeeba034c77d5a
Freud Ovando Lara <freud5310@gmail.com>
2024-04-03 01:24:50 -0600

    i made some styles for the header

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,26 @@
-import 'boxicons'
+// import 'boxicons'
+
+const darkMode = document.getElementById('input-dark-mode');
+
+darkMode.addEventListener('click', () => {
+console.log("hola");    
+})
+
+
+const texto = "Hola mi nombre es Freud Ovando y soy full Stack Developer";
+
+let index = 0;
+
+function escribirTexto() { 
+  document.getElementById('text-profile').innerText = texto.slice(0, index);
+  index++;
+  if (index > texto.length) {
+    clearInterval(intervalo);
+    setTimeout(() => {
+      index = 0;
+      intervalo = setInterval(escribirTexto, 100);
+    }, 2500); 
+  }
+}
+
+let intervalo = setInterval(escribirTexto, 100);
